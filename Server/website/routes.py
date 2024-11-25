2300be53295f1d3f346b640d03a030ea528e1372
evan-poster <evan.poster@gmail.com>
2024-11-25 10:46:27 -0500

    added company_list.html template; updated routes

--- Server/website/routes.py
+++ Server/website/routes.py
@@ -7,27 +7,38 @@ from flask import request
 def create_app():
     # reminder make a template folder for the html so it can go find the html in the template folder
     # put justin html file here debug.html is a debugging file
-    return render_template("debug.html", debug=True)
+    context = {}
+    return render_template("base.html", Debug=True, **context)
 
 
 # needs justin html to do the post
-app.route("/", methods=["POST", "GET"])
+# app.route("/", methods=["POST", "GET"])
 
+@app.route("/c")
+def companies():
+    # companies = 
+    return render_template("company_list.html")
 
-def add_customer():
+@app.route("/c/new", methods=["POST", "GET"])
+def new_company():
     if request.method == "POST":
         pass
-        username = request.form.get()  # store the form name
-        comment = request.form.get()  # store the form name
-        upload_picture = request.form.get()  # store the form name
-        ratings = request.form.get()  # store the form name
+        name = request.form.get()  # store the form name
+
+# def add_customer():
+#     if request.method == "POST":
+#         pass
+#         username = request.form.get()  # store the form name
+#         comment = request.form.get()  # store the form name
+#         upload_picture = request.form.get()  # store the form name
+#         ratings = request.form.get()  # store the form name
 
 
-db."""name of collection""".insertOne():
-{
-    "username": username,
-    "comment": comment,
-    "upload_picture": upload_picture,  # might need to make this true or false
-    "ratings": ratings
+# db."""name of collection""".insertOne():
+# {
+#     "username": username,
+#     "comment": comment,
+#     "upload_picture": upload_picture,  # might need to make this true or false
+#     "ratings": ratings
 
-}
+# }
